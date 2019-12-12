1558d667965c734b143e1f1dee3f151427bddeaf
KIrilldeveloper48 <58586831+KIrilldeveloper48@users.noreply.github.com>
2019-12-12 15:32:33 +0300

    Добавлен функционал слайдера

--- gostnorm/js/main.js
+++ gostnorm/js/main.js
@@ -1,6 +1,34 @@
+var slides = document.querySelectorAll("#slides .first-screen_slider-item");
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide, 20000);
+
+function nextSlide() {
+  goToSlide(currentSlide + 1);
+}
+
+function previousSlide() {
+  goToSlide(currentSlide - 1);
+}
+
+function goToSlide(n) {
+  slides[currentSlide].className = "first-screen_slider-item";
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].className = "first-screen_slider-item show";
+}
+
+var next = document.getElementById("next");
+var previous = document.getElementById("back");
+
+next.onclick = function() {
+  nextSlide();
+};
+previous.onclick = function() {
+  previousSlide();
+};
+
 $("#nav-menu_item-1").click(function() {
-    $("#sub-menu-1").toggleClass("sub-menu_active");
-  });
+  $("#sub-menu-1").toggleClass("sub-menu_active");
+});
 $("#nav-menu_item-2").click(function() {
   $("#sub-menu-2").toggleClass("sub-menu_active");
 });
