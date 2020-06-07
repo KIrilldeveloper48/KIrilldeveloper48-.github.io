c0359c22148ba910a72a4a4b896174086bc9a041
kirill <pkv48@yandex.ru>
2020-06-07 19:01:16 +0300

    Put the slider in a separate module. Rewrote the algorythm for filtering

--- Lesson_Project_KekstaGram/js/filtering.js
+++ Lesson_Project_KekstaGram/js/filtering.js
@@ -1,174 +1,158 @@
 (function () {
-  var currentFilter;
-  var transparency;
-  var imgPreview = window.uploadOverlay.querySelector(".img-upload__preview>img");
-  var filters = window.uploadOverlay.querySelectorAll(".effects__radio");
-  let levelPin = window.uploadOverlay.querySelector(".effect-level__pin");
-  let levelDepth = window.uploadOverlay.querySelector(".effect-level__depth");
 
-  var filtersOnClick = function () {
-    let levelContainer = window.uploadOverlay.querySelector(".effect-level");
-
-    for (var i = 0; i < filters.length; i++) {
-      filters[i].addEventListener("click", function (evt) {
-        currentFilter = getFilter(evt);
-        cleaning();
-
-        if (currentFilter != "effect-none") levelShow();
-        else levelHidden();
-      });
-    };
-
-    var getFilter = function (evt) {
-      return evt.originalTarget.id
+  let currentFilter;
+  let listEffects = window.uploadOverlay.querySelector(".effects__list")
+  listEffects.addEventListener("click", function (evt) {
+    if (evt.target.classList.contains("effects__preview")) {
+      onClickMouse(evt)
     }
+  });
 
-    var cleaning = function () {
-      imgPreview.style.filter = "";
-      imgPreview.className = "";
-      transparency = 0;
-      levelPin.style.left = 0;
-      levelDepth.style.width = 0;
-    };
+  var onClickMouse = function (evt) {
+    currentFilter = getFilter(evt);
+    cleaning();
 
-    var levelShow = function () {
-      levelContainer.classList.remove("hidden");
-    };
+    if (currentFilter.classList.contains("effects__preview--none") == false) {
+      levelShow();
+    } else levelHidden();
 
-    var levelHidden = function () {
-      levelContainer.classList.add("hidden");
-    };
-  }();
+  }
+
+  var getFilter = function (evt) {
+    return evt.target;
+  };
+
+  let imgPreview = window.uploadOverlay.querySelector(
+    ".img-upload__preview>img"
+  );
+  let levelDepth = window.uploadOverlay.querySelector(".effect-level__depth");
+  let levelPin = window.uploadOverlay.querySelector(".effect-level__pin");
+
+  var cleaning = function () {
+    imgPreview.style.filter = "";
+    imgPreview.className = "";
+    transparency = 0;
+    levelPin.style.left = 0;
+    levelDepth.style.width = 0;
+  };
+
+  let levelContainer = window.uploadOverlay.querySelector(".effect-level");
+
+  var levelShow = function () {
+    levelContainer.classList.remove("hidden");
+  };
+
+  var levelHidden = function () {
+    levelContainer.classList.add("hidden");
+  };
 
   //-------------------------------------------------------------------
 
-  var filtering = function () {
-    let levelLine = window.uploadOverlay.querySelector(".effect-level__line");
+  var filtering = function (position) {
+    levelDepth.style.width = position + "%";
+    rewriteEffectValue(position);
+    let effect = getEffect(position)
+    addEffect(effect);
+    imgAddClass();
+  };
 
-    levelPin.addEventListener('mousedown', function (evt) {
-      evt.preventDefault();
+  var rewriteEffectValue = function (transparency) {
+    let effectsValue = window.uploadOverlay.querySelector(
+      ".effect-level__value"
+    );
+    effectsValue.value = transparency;
+  };
 
-      let levelFieldset = window.uploadOverlay.querySelector(".effect-level");
+  var getEffect = function (transparency) {
+    let effects = {
+      grayscale: {
+        multiplier: 0.01,
+        unit: ''
+      },
+      sepia: {
+        multiplier: 0.01,
+        unit: ''
+      },
+      invert: {
+        multiplier: 1,
+        unit: '%'
+      },
+      blur: {
+        multiplier: 0.03,
+        unit: 'px'
+      },
+      brightness: {
+        multiplier: 0.03,
+        unit: ''
+      },
+    };
 
-      let startsCoord = {
-        x: evt.clientX
-      };
+    let nameEffect = getObj();
+    let value = transparency * effects[nameEffect].multiplier;
 
-      var onMouseMove = function (moveEvt) {
-        moveEvt.preventDefault();
-
-        let shift = {
-          x: startsCoord.x - moveEvt.clientX
-        };
-
-        startsCoord = {
-          x: moveEvt.clientX
-        };
-
-        if ((levelPin.offsetLeft - shift.x) > 0 && (levelPin.offsetLeft - shift.x) < 455) {
-          levelPin.style.left = (levelPin.offsetLeft - shift.x) + 'px';
-        }
-
-        transparency = getTransparency();
-        levelDepth.style.width = transparency + "%"
-        rewriteEffectValue(transparency);
-        addEffects();
-        imgAddClass();
-      };
-
-      var onMouseUp = function (upEvt) {
-        upEvt.preventDefault();
-        levelFieldset.removeEventListener('mousemove', onMouseMove);
-        levelFieldset.removeEventListener('mouseup', onMouseUp);
-      };
-      levelFieldset.addEventListener('mousemove', onMouseMove);
-      levelFieldset.addEventListener('mouseup', onMouseUp);
-    })
-
-    var getTransparency = function () {
-      return Math.floor((100 / levelLine.clientWidth) * (levelPin.offsetLeft + levelPin.clientWidth / 2));
+    if (nameEffect == 'brightness') {
+      value++
     }
 
-    var rewriteEffectValue = function () {
-      let effectsValue = window.uploadOverlay.querySelector(".effect-level__value");
-      effectsValue.value = transparency;
-    };
+    let unit = effects[nameEffect].unit;
+    return nameEffect + "(" + value + unit + ")";
+  };
 
-    //Creating the dictionary for matching filters and effects
-    var getMap = function () {
-      let effects = [
-        [" ", 1, ""],
-        ["grayscale", 0.01, ""],
-        ["sepia", 0.01, ""],
-        ["invert", 1, "%"],
-        ["blur", 0.03, "px"],
-        ["brightness", 0.03, ""],
-      ];
+  var getObj = function () {
+    let currentStyle = getComputedStyle(currentFilter).filter;
+    return currentStyle.slice(0, currentStyle.indexOf('('))
+  };
 
-      let map = new Map();
+  var addEffect = function (effect) {
+    imgPreview.style.filter = effect;
+  };
 
-      for (var i = 0; i < filters.length; i++) {
-        map.set(filters[i].id, effects[i]);
-      }
-      return map
-    };
+  var imgAddClass = function () {
+    let nameClass = currentFilter.classList[1]
+    imgPreview.classList.add(nameClass);
+  };
 
-    var addEffects = function () {
-      //Finding the effect index that matches the filter in the dictionary
-      effectArr = getMap().get(currentFilter);
-      imgPreview.style.filter = setFilter(
-        effectArr[0],
-        effectArr[1],
-        effectArr[2]
-      );
-    };
-
-    var setFilter = function (effect, value, unit) {
-      transparency *= value;
-
-      if (effect === "brightness") {
-        transparency++;
-      }
-
-      return effect + "(" + transparency + unit + ")";
-    };
-
-    var imgAddClass = function () {
-      let nameClass = "effects__preview--" + currentFilter.slice(7, currentFilter.length);
-      imgPreview.classList.add(nameClass);
-    };
-
+  var callSlider = function () {
+    window.slider(filtering);
   }();
 
-  //----------------------------------------------
-  var scaleControl = function () {
-    let resizePlus = window.uploadOverlay.querySelector(".scale__control--bigger");
-    let resizeMinus = window.uploadOverlay.querySelector(".scale__control--smaller");
-    let resizeValue = window.uploadOverlay.querySelector(".scale__control--value");
-    let scaleValue = 100;
+})()
 
-    resizePlus.addEventListener("click", function () {
-      if (resizeValue.value != "100%") {
-        scaleValue += 25;
-        addResizeValue();
-        addResizeImg();
-      }
-    });
+//----------------------------------------------
+var scaleControl = (function () {
+  let resizePlus = window.uploadOverlay.querySelector(
+    ".scale__control--bigger"
+  );
+  let resizeMinus = window.uploadOverlay.querySelector(
+    ".scale__control--smaller"
+  );
+  let resizeValue = window.uploadOverlay.querySelector(
+    ".scale__control--value"
+  );
+  let scaleValue = 100;
 
-    resizeMinus.addEventListener("click", function () {
-      if (resizeValue.value != "25%") {
-        scaleValue -= 25;
-        addResizeValue();
-        addResizeImg();
-      }
-    });
+  resizePlus.addEventListener("click", function () {
+    if (resizeValue.value != "100%") {
+      scaleValue += 25;
+      addResizeValue();
+      addResizeImg();
+    }
+  });
 
-    var addResizeValue = function () {
-      resizeValue.value = String(scaleValue) + "%";
-    };
+  resizeMinus.addEventListener("click", function () {
+    if (resizeValue.value != "25%") {
+      scaleValue -= 25;
+      addResizeValue();
+      addResizeImg();
+    }
+  });
 
-    var addResizeImg = function () {
-      imgPreview.style.transform = "scale" + "(" + resizeValue.value + ")";
-    };
-  }();
+  var addResizeValue = function () {
+    resizeValue.value = String(scaleValue) + "%";
+  };
+
+  var addResizeImg = function () {
+    imgPreview.style.transform = "scale" + "(" + resizeValue.value + ")";
+  };
+
 })();
