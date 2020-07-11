6209b2d32cb384646603f83aaac5e40f4308cdc8
kirill <pkv48@yandex.ru>
2020-07-11 21:18:49 +0300

    Перенёс запуск отрисовки пинов в модуль data. Отслеживание клика будет навешиваться только после получения данных с сервера. Функция отрисовки пинов теперь будет принимать на вход параметр data (массив с данными для отрисовки). Это нужно чтобы в функцию можно было передавать разные массивы для отрисовки

--- Lesson_Project_KeksoBooking/js/data.js
+++ Lesson_Project_KeksoBooking/js/data.js
@@ -1,11 +1,17 @@
 "use strict";
 (function () {
-
-  window.load(function (data) {
+  window.load(
+    function (data) {
       window.arrayAD = data;
+      window.mainPin.addEventListener("mouseup", function () {
+        window.genMapPin(data);
+      });
+      window.mainPin.addEventListener("mouseup", function () {
+        window.addClickPin();
+      });
     },
     function (message) {
       console.error(message);
-    });
-
+    }
+  );
 })();
--- Lesson_Project_KeksoBooking/js/pin.js
+++ Lesson_Project_KeksoBooking/js/pin.js
@@ -1,32 +1,28 @@
 "use strict";
 (function () {
-
-  var genMapPin = function () {
+  window.genMapPin = function (arrayAD) {
     let template = document.querySelector("#pin").content;
     let mapPin = template.querySelector(".map__pin");
     let mapPins = document.querySelector(".map__pins");
     let axisShift = {
       y: mapPin.clientHeight,
-      x: mapPin.clientWidth / 2
-    }
+      x: mapPin.clientWidth / 2,
+    };
 
-    for (var i = 0; i < window.arrayAD.length; i++) {
+    for (var i = 0; i < arrayAD.length; i++) {
       let clone = mapPin.cloneNode(true);
       let avatar = clone.querySelector("img");
-      let left = String(window.arrayAD[i].location.x - axisShift.x) + "px";
-      let top = String(window.arrayAD[i].location.y - axisShift.y) + "px";
+      let left = String(arrayAD[i].location.x - axisShift.x) + "px";
+      let top = String(arrayAD[i].location.y - axisShift.y) + "px";
 
       //Setting the parameters
       clone.style.left = left;
       clone.style.top = top;
-      clone.id = 'pin-' + i;
+      clone.id = "pin-" + i;
       avatar.src = arrayAD[i].author.avatar;
       avatar.alt = arrayAD[i].offer.title;
 
       mapPins.append(clone);
     }
   };
-
-  window.mainPin.addEventListener("mouseup", genMapPin);
-
 })();
