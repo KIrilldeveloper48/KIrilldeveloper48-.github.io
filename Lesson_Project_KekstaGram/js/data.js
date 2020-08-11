f96339b762cd718283c877cdebc351ef91a1416c
Kirill <developerpopov@yandex.ru>
2020-08-11 16:49:27 +0300

    Удалил модуль export
    за ненадобностью (он содержал один единственный элемент)

--- Lesson_Project_KekstaGram/js/data.js
+++ Lesson_Project_KekstaGram/js/data.js
@@ -1,9 +1,13 @@
 (function () {
+  //Запускаем функцию из модуля backend
   window.load(
     function (data) {
+      //Присваиваем глобальной переменной полученные данные 
       window.dataOriginal = data;
+      //Запускаем фунции по отрисовки фотографии и превью
       window.picture(data);
       window.preview(data);
+      //Запускаем функцию для сортировки фотографий
       window.imgFilters();
     },
     function (message) {
